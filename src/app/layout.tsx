0fef744a5a2199033056c31a27ef55011ef1d30b
D.Chinh <chinh6803@gmail.com>
2023-12-17 16:54:41 +0700

    layout-next-js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 // import './globals.css'
-
+// layout full every page example header - footer 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <header><h1>Header</h1></header>
       <body className={inter.className}>{children}</body>
+      <footer><h1>Footer</h1></footer>
     </html>
   )
 }
